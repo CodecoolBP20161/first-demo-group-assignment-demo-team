79d2fd028266aeadf70f27b8ccf3cbe6d0ab8803
Tomi <tomi@192.168.1.2>
2016-05-12 01:53:55 +0200

    Reformatted with pep8

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import unittest
 
 # Here's our "unit".
+
+
 def IsOdd(n):
     return n % 2 == 1
 
 # Here's our "unit tests".
+
+
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
@@ -12,6 +16,7 @@ class IsOddTests(unittest.TestCase):
 
     def testTwo(self):
         self.failIf(IsOdd(2))
+
 
 def main():
     unittest.main()
